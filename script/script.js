cc40b8ba07b43a1fd303dde232c9a63de649df72
Habib0088 <sarkerbatch2k19@gmail.com>
2025-09-05 00:10:33 +0600

    info button done

--- script/script.js
+++ script/script.js
@@ -3,21 +3,23 @@ let url = "https://openapi.programming-hero.com/api/levels/all";
 fetch(url)
   .then((res) => res.json())
   .then((book) => showData(book.data));
-  // remove active class from all button
-  let removeActive=()=>{
-    let activebuttonClear=document.querySelectorAll('.common-btn');
-    activebuttonClear.forEach((colorBtn)=>{
-      colorBtn.classList.remove('active');
-    })
-    
-  } 
+// remove active class from all button
+let removeActive = () => {
+  let activebuttonClear = document.getElementsByClassName("common-btn");
+  for (let actbtn of activebuttonClear) {
+    actbtn.classList.remove("active");
+  }
+  // activebuttonClear.forEach((colorBtn)=>{
+  //   colorBtn.classList.remove('active');
+  // })
+};
 
 const loadLiveWord = (id) => {
   let url = `https://openapi.programming-hero.com/api/level/${id}`;
-  let activeButton=document.getElementById(`activeBtn-${id}`);
-  removeActive()
-  activeButton.classList.add('active');
-  
+  let activeButton = document.getElementById(`activeBtn-${id}`);
+  removeActive();
+  activeButton.classList.add("active");
+
   // console.log(url);
   fetch(url)
     .then((respon) => respon.json())
@@ -28,39 +30,97 @@ const showWord = (words) => {
   let wordArea = document.getElementById("word-container");
   wordArea.innerHTML = "";
 
-  if(words.length===0){
+  if (words.length === 0) {
     wordArea.innerHTML = `
     <div class="default-text  col-span-full text-center p-10 space-y-9 bg-[#79716B10]">
      <img src="./assets/alert-error.png" alt="" class="mx-auto" >
         <p class="text-2xl font-semibold hind ">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
         <h2 class="text-6xl font-bold hind">নেক্সট Lesson এ যান</h2>
       </div>
-    `
-  return
-
+    `;
+    return;
   }
+
   words.forEach((word) => {
     let wordElement = document.createElement("div");
 
     // console.log(word);
-    
+
     wordElement.innerHTML = `
      <div class="card shadow p-4 h-full">
           <div class="card-body items-center text-center space-y-4 ">
             <h2 class="card-title text-2xl">${word.word}!</h2>
            
-            <p class="text-[#18181B] text-2xl ">Meaning: ${word.meaning ? word.meaning : '<span class="text-[red] "> "অর্থ পাওয়া যায়নি"</span>'} </p>
-            <p class="text-[#18181B] text-2xl">Pronouciation: ${word.pronunciation ? word.pronunciation : '<span class="text-[red]"> "উচ্চারন পাওয়া যায়নি"</span>'} </p>
+            <p class="text-[#18181B] text-2xl ">Meaning: ${
+              word.meaning
+                ? word.meaning
+                : '<span class="text-[red] "> "অর্থ পাওয়া যায়নি"</span>'
+            } </p>
+            <p class="text-[#18181B] text-2xl">Pronouciation: ${
+              word.pronunciation
+                ? word.pronunciation
+                : '<span class="text-[red]"> "উচ্চারন পাওয়া যায়নি"</span>'
+            } </p>
             <div class="card-actions flex justify-between w-full">
-              <div><i class="fa-solid fa-circle-question"></i></div>
-              <div><i class="fa-solid fa-volume-high"></i></div>
+              <button onclick="wordDetails(${
+                word.id
+              })"><i class="fa-solid fa-circle-question"></i></button>
+              <button><i class="fa-solid fa-volume-high"></i></button>
             </div>
           </div>
         </div>
     `;
+    // console.log(word);
+
     wordArea.appendChild(wordElement);
   });
 };
+
+// Word Details
+
+let wordDetails = (id) => {
+  let wordUrl = `https://openapi.programming-hero.com/api/word/${id}`;
+
+  fetch(wordUrl)
+    .then((res) => res.json())
+    .then((data) => showWordDetails(data.data));
+};
+let showWordDetails=(data)=>{
+  console.log(data);
+  
+
+  my_modal_5.showModal()
+
+  let wordArea=document.getElementById('my_modal_5');
+  wordArea.innerHTML=`
+
+  <div  class="p-10 space-y-5 bg-white rounded-md">
+<div class="inner border-2 border-gray-50 space-y-2 p-4 rounded-md">
+  <h2 class="text-2xl font-bold">${data.word} <span>(<i class="fa-solid fa-microphone-lines"></i>${data.pronunciation})</span></h2>
+<p class="font-bold">Meaning</p>
+<p class="font-semibold">${data.meaning ? data.meaning : `<span class="text-red-600">খুজে পাওয়া যায় নি</span>`}</p>
+<p class="font-bold">example</p>
+<p class="text-white">${data.sentence}</p>
+<p class="font-bold">সমার্থক শব্দগুলো</p>
+<div class="similarContainer flex gap-4 ">
+  <h3 class="bg-blue-100 py-2 px-6 rounded-md">${data.synonyms[0]}</h3>
+  <h3 class="bg-blue-100 py-2 px-6 rounded-md">${data.synonyms[1]}</h3>
+  <h3 class="bg-blue-100 py-2 px-6 rounded-md">${data.synonyms[2]}</h3>
+</div>
+</div>
+<button class="bg-[#422AD5] py-3 px-8 rounded-md text-white">Complete Learning</button>
+     <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+</div>
+  
+  `
+  document.getElementById('my_modal_5').showModal()
+
+}
 
 const showData = (lessions) => {
   lessions.forEach((lession) => {
@@ -77,4 +137,3 @@ const showData = (lessions) => {
     container.appendChild(area);
   });
 };
-
