8b4d914fcc68c237bec5b4deee1e0559437fdbc8
Habib0088 <sarkerbatch2k19@gmail.com>
2025-09-04 17:01:33 +0600

    error done

--- script/script.js
+++ script/script.js
@@ -15,15 +15,30 @@ const loadLiveWord = (id) => {
 const showWord = (words) => {
   let wordArea = document.getElementById("word-container");
   wordArea.innerHTML = "";
+
+  if(words.length===0){
+    wordArea.innerHTML = `
+    <div class="default-text  col-span-full text-center p-10 space-y-9 bg-[#79716B10]">
+     <img src="./assets/alert-error.png" alt="" class="mx-auto" >
+        <p class="text-2xl font-semibold hind ">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="text-6xl font-bold hind">নেক্সট Lesson এ যান</h2>
+      </div>
+    `
+  return
+
+  }
   words.forEach((word) => {
     let wordElement = document.createElement("div");
 
+    console.log(word);
+    
     wordElement.innerHTML = `
      <div class="card shadow p-4">
-          <div class="card-body items-center text-center">
+          <div class="card-body items-center text-center space-y-4">
             <h2 class="card-title text-2xl">${word.word}!</h2>
-            <p>pronouciation</p>
-            <p class="text-[#18181B] text-2xl">${word.pronunciation}</p>
+           
+            <p class="text-[#18181B] text-2xl ">Meaning: ${word.meaning ? word.meaning : '<span class="text-[red]"> "অর্থ পাওয়া যায়নি"</span>'} </p>
+            <p class="text-[#18181B] text-2xl">Pronouciation: ${word.pronunciation ? word.pronunciation : '<span class="text-[red]"> "উচ্চারন পাওয়া যায়নি"</span>'} </p>
             <div class="card-actions flex justify-between w-full">
               <div><i class="fa-solid fa-circle-question"></i></div>
               <div><i class="fa-solid fa-volume-high"></i></div>
